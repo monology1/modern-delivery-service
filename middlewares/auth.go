6f2163d4c3d5065d4cd84b5417110973355e582c
monology1 <monology_za@hotmail.com>
2024-07-02 19:51:23 +0700

    [integrated]: jwt,hash, and middlewares

---
+++ middlewares/auth.go
@@ -0,0 +1,26 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"modern-delivery-service/utils"
+	"net/http"
+)
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "No token found"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Invalid token"})
+		return
+	}
+
+	context.Set("userId", userId)
+	context.Next()
+}
